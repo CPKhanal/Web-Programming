adea701e442742ae634569fc03b7707a4e4cb4f3
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2024-02-16 16:59:02 -0500

    week 6

---
+++ Week 6/script.js
@@ -0,0 +1,70 @@
+// console.log("This is our javascript");
+// // This  is single line comment
+
+// let age = 21;
+// let name = "CP Khanal";
+// console.log("Hello my name is ", name );
+// console.log("I am %d years old", age);
+// document.getElementById("p1").innerHTML = "Hello this is " + name;
+// document.getElementById("p2").innerHTML = `Hello I am ${age} years old`;
+
+// let a=20;
+// let b=30;
+// console.log("The sum is", a+b);
+// console.log("The difference is", b-a);
+// console.log("The multiplication is", a*b);
+// console.log("The division is", b/a);
+// console.log("The  modulo division is", b%a);
+
+// console.log(2+40-60/2*6%2);
+// console.log((2+40)-(60/2)*(6%2));
+
+// console.log(1+2*3+4);
+// console.log(1+2*(3+4));
+
+// let username = window.prompt("What is your name");
+// console.log(username);
+// document.getElementById("p1").innerHTML = `Hello ${username}`;
+
+// let x = 3.14;
+// let y = 2;
+// let z = 34;
+// let maximum;
+// x  = Math.floor(x);
+// y = Math.pow(y,3);
+// y = Math.sqrt(4);
+// maximum = Math.max(x,y,z);
+// console.log(maximum);
+
+// let a =112;
+// if(a==12){
+//     console.log("Yes! it's 12");
+// }
+// else{
+//     console.log("NO! it's not 12");
+// }
+
+//TO check if the number is even or odd
+// let number = window.prompt("Enter the number");
+// if(number%2==0){
+//     console.log("This is even");
+// }
+// else{
+//     console.log("number is odd");
+// }
+
+//To convert celcious to farnheight
+// let Celsius = 37;
+// Fahrenheit = (Celsius * 9/5) + 32;
+// console.log(Fahrenheit);
+
+//Another problem
+let price =1000;
+let age =65;
+if(age>=65){
+    let a=1000-(15/100)*1000;
+    console.log("15% discount is applied! You have to pay "+ a);
+}
+else{
+    console.log("You have to pay 1000")
+}
